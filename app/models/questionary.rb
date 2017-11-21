95cf1c72736245cafab947a0da916f3503828b7e
Saki Ohta <sakiohta@hotmail.com>
2017-11-21 06:32:15 +0000

    first commit

---
+++ app/models/questionary.rb
@@ -0,0 +1,7 @@
+class Questionary < ApplicationRecord
+	has_many :questionary_item
+	has_many :questionary_result
+
+	validates :title, :description, :deadline, presence:
+	{message:'は、必須項目です。 '}
+end
